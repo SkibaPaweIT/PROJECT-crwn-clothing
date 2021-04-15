889a050ed087d22bf9e7de373cc828da1409e033
SkibaPawelT <skibapawelt@gmail.com>
2021-04-15 12:04:07 +0200

    broken our homepage into smalle components. finished styling our menu-items
    ;

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import './directory.styles.scss';
+import MenuItem from '../menu-item/menu-item.component'
+
+class Directory extends React.Component {
+    constructor(){
+        super();
+
+        this.state = {
+            sections: [
+                {
+                  title: 'hats',
+                  imageUrl: 'https://th.wallhaven.cc/small/rd/rd7drw.jpg',                 
+                  id: 1,
+                  linkUrl: 'shop/hats'
+                },
+                {
+                  title: 'jackets',
+                  imageUrl: 'https://th.wallhaven.cc/small/rd/rd3pjw.jpg',
+                  id: 2,
+                  linkUrl: 'shop/jackets'
+                },
+                {
+                  title: 'sneakers',
+                  imageUrl: 'https://th.wallhaven.cc/small/rd/rddgwm.jpg',
+                  id: 3,
+                  linkUrl: 'shop/sneakers'
+                },
+                {
+                  title: 'womens',
+                  imageUrl: 'https://th.wallhaven.cc/small/z8/z8mq8y.jpg',
+                  size: 'large',
+                  id: 4,
+                  linkUrl: 'shop/womens'
+                },
+                {
+                  title: 'mens',
+                  imageUrl: 'https://w.wallhaven.cc/full/z8/wallhaven-z887yw.jpg',
+                  size: 'large',
+                  id: 5,
+                  linkUrl: 'shop/mens'
+                }
+              ]
+        }
+
+
+    }
+
+    render() {
+        return (
+            <div className='directory-menu'>
+                {
+                    this.state.sections.map(({title, imageUrl, id, size})=> (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+
+                    ))
+                }
+            </div>
+        )
+    }
+
+}
+
+export default Directory;
