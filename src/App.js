56297af37371953387dae5d1ab32c75d75de2aa5
SkibaPawelT <skibapawelt@gmail.com>
2021-04-19 13:02:04 +0200

    Adding header. Styling it a little bit

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import './App.css';
 import HomePage from './pagges/homepage/homepage.component';
 import {Switch ,Route } from 'react-router-dom';
 import ShopPage from './pagges/shop/shop.component';
+import Header from './components/header/header.component.jsx'
 
 function App() {
   return (
     <div>
+    <Header />
       <Switch> {/* It will not render anything else */}
         <Route exact path='/' component={HomePage}/> {/* if we don't use exact it will get every route where it's slash */}
         <Route exact path='/shop' component={ShopPage}/>
       </Switch>
     </div>
-    
-
   );
 }
 
