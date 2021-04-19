d2fc84a06e8dd7204e5a98e10b33753888fb96d5
SkibaPawelT <skibapawelt@gmail.com>
2021-04-19 12:42:43 +0200

    updating our app witha a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pagges/homepage/homepage.component';
-import {Switch ,Route , withRouter} from 'react-router-dom';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import {Switch ,Route } from 'react-router-dom';
+import ShopPage from './pagges/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch> {/* It will not render anything else */}
         <Route exact path='/' component={HomePage}/> {/* if we don't use exact it will get every route where it's slash */}
-        <Route exact path='/shop/hats' component={HatsPage}/>
+        <Route exact path='/shop' component={ShopPage}/>
       </Switch>
     </div>
     
