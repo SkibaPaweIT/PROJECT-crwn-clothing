dc6de75f22bced7f6ca1dcfd3f87bc0eca39e830
SkibaPawelT <skibapawelt@gmail.com>
2021-04-24 12:57:07 +0200

    handling with simple errors related with sign in with google button (change typ from submit to button)

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -49,7 +49,7 @@ class SignIn extends React.Component{
                 <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} label="password" required/>
                 <div className='buttons'>
                     <CustomButton type='submit'> Sign in </CustomButton>
-                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>{' '} Sign in with Google {' '} </CustomButton>
+                    <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>{' '} Sign in with Google {' '} </CustomButton>
                 </div>
 
             </form>
